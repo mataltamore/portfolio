eed2b841701d0e95fdd9517a5b604f73dd1b4a0b
Matteo Altamore <maltamore@deloitte.it>
2022-04-01 21:35:59 +0200

    initial configs

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import Main from "../Main/Main";
+import Sidebar from "../Sidebar/Sidebar";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+};
+
+export default App;
